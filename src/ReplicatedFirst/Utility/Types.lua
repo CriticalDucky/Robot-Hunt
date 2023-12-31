c6dce312be5317178d90c5ef18da143287867038
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-12-31 15:54:10 -0500

    Remove uncessary data

--- src/ReplicatedFirst/Utility/Types.lua
+++ src/ReplicatedFirst/Utility/Types.lua
@@ -11,10 +11,6 @@ export type ItemAccessory = {
 	type: number,
 }
 
-export type ItemFurniture = {
-	type: number,
-}
-
 type Use = Fusion.Use
 
 export type ItemCategory = { InventoryItem }
@@ -39,18 +35,10 @@ export type PlayerPersistentData = {
 	currency: {
 		money: number,
 	},
-	home: {
-		selected: string?,
-		server: ServerIdentifier?,
-	},
 	inventory: {
 		accessories: { [string]: ItemAccessory? },
-		furniture: { [string]: ItemFurniture? },
-		homes: { [string]: ItemHome? },
 	},
 	settings: {
-		findOpenWorld: boolean,
-		homeLock: number,
 		musicVolume: number,
 		sfxVolume: number,
 	},
@@ -59,8 +47,6 @@ export type PlayerPersistentData = {
 export type PlayerPersistentDataPublic = {
 	inventory: {
 		accessories: { [string]: ItemAccessory? },
-		furniture: { [string]: ItemFurniture? },
-		homes: { [string]: ItemHome? },
 	},
 	settings: {
 		homeLock: number,
@@ -89,77 +75,13 @@ export type TimeInfo = number | (
 	sec: number?,
 }
 
-export type PartyUnit = {
-	partyType: UserEnum,
-	halfHourId: number,
-	time: TimeRange,
-}
-
-export type ItemHome = {
-	type: number,
-}
-
-export type HomeServerInfo = {
-	serverCode: string,
-	privateServerId: string,
-}
-
 export type InventoryItem = {
 	id: string,
 	itemCategory: UserEnum,
 	itemEnum: string | number,
-	placedItems: {}?,
 	permanent: boolean?,
 }
 
-export type PlacedItem = {
-	itemId: string,
-	pivotCFrame: CFrame | { [any]: any },
-}
-
 export type Promise = typeof(Promise.new(function() end))
-
-export type CatalogMinigameData = { ServerIdentifier }
-
-export type CatalogPartyData = { ServerIdentifier }
-
-export type CatalogWorldData = { [string]: ServerIdentifier }
-
-export type CatalogWorldLocationList = { [string]: { placeId: number } }
-
-export type LocationType = "forest" | "town"
-
-export type MinigameType = "fishing" | "gatherer"
-
-export type PartyType = "beach"
-
-export type ServerIdentifier = {
-	accessCode: string,
-	privateServerId: string,
-}
-
-export type ServerInfoHome = {
-	homeOwner: number,
-}
-
-export type ServerInfoLocation = {
-	world: number,
-}
-
---[[
-	The table returned from `Player:GetJoinData().TeleportData` if such data exists.
-
-	---
-
-	#### Fields
-	* `associatedWorld` The world the player is associated with. This represents the player's world if they are not in a
-	location.
-	* `locationFrom` The location the player is teleporting from. This determines the entry point the player will spawn
-	at when joining a location.
-]]
-export type TeleportData = {
-	associatedWorld: number?,
-	locationFrom: string?,
-}
 
 return nil
