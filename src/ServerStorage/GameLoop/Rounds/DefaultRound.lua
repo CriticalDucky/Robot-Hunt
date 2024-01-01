494e6d4eeaa9d3ddf70c19372c656b787652c5c7
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-01 00:58:36 -0500

    Add default round process

--- src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
+++ src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
@@ -1,7 +1,14 @@
+local ServerStorage = game:GetService "ServerStorage"
+
+local Modules = require(ServerStorage.GameLoop.Modules)
+
 local DefaultRound = {}
 
 function DefaultRound.begin()
-    
+	return Modules.DefaultRound.Hiding
+		.begin()
+		:andThenCall(Modules.DefaultRound.PhaseOne.begin)
+        :andThenCall(Modules.DefaultRound.PhaseTwo.begin)
 end
 
 return DefaultRound
