d44c24e48fd74cf04db5ebb1d7e19a0e20767982
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-01 22:40:29 -0500

    Add RoundData editing

--- src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
+++ src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
@@ -1,10 +1,18 @@
 local ServerStorage = game:GetService "ServerStorage"
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
 
-local Modules = require(ServerStorage.GameLoop.Modules)
+local GameLoop = ServerStorage.GameLoop
+
+local Enums = require(ReplicatedFirst.Enums)
+local Modules = require(GameLoop.Modules)
+-- local Actions = require(GameLoop.Actions)
+local RoundData = require(GameLoop.RoundData)
 
 local DefaultRound = {}
 
 function DefaultRound.begin()
+	RoundData.data.currentRoundType = Enums.RoundType.defaultRound
+
 	return Modules.DefaultRound.Hiding
 		.begin()
 		:andThenCall(Modules.DefaultRound.PhaseOne.begin)
