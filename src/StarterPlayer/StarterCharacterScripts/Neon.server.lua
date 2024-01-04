e73c1358d57324dc220791a3899d73c9380b27fa
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-04 18:47:08 -0500

    Add neon script

---
+++ src/StarterPlayer/StarterCharacterScripts/Neon.server.lua
@@ -0,0 +1,29 @@
+local HIDERS_COLOR = Color3.fromRGB(79, 144, 219)
+local SEEKERS_COLOR = Color3.fromRGB(224, 91, 93)
+local LOBBY_COLOR = Color3.fromRGB(223, 94, 223)
+
+local players = game:GetService("Players")
+local teams = game:GetService("Teams")
+
+local character = script.Parent
+local player = players:GetPlayerFromCharacter(character)
+
+local hidersTeam = teams:WaitForChild("Hiders")
+local seekersTeam = teams:WaitForChild("Seekers")
+local lobbyTeam = teams:WaitForChild("Lobby")
+
+local neon = character:WaitForChild("Neon")
+
+function changeNeon()
+	if player.Team == hidersTeam then
+		neon.Color = HIDERS_COLOR
+	elseif player.Team == seekersTeam then
+		neon.Color = SEEKERS_COLOR
+	elseif player.Team == lobbyTeam then
+		neon.Color = LOBBY_COLOR
+	end
+end
+
+changeNeon()
+
+player:GetPropertyChangedSignal("Team"):Connect(changeNeon)
