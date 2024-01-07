903dd245e65b30684657b537474aef36fac838ff
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-06 21:34:21 -0500

    Change team names

--- src/StarterCharacterScripts/Neon.server.lua
+++ src/StarterCharacterScripts/Neon.server.lua
@@ -1,5 +1,5 @@
-local HIDERS_COLOR = Color3.fromRGB(99, 153, 213)
-local SEEKERS_COLOR = Color3.fromRGB(220, 116, 118)
+local REBELS_COLOR = Color3.fromRGB(99, 153, 213)
+local HUNTERS_COLOR = Color3.fromRGB(220, 116, 118)
 local LOBBY_COLOR = Color3.fromRGB(220, 122, 220)
 
 local players = game:GetService("Players")
@@ -8,17 +8,17 @@ local teams = game:GetService("Teams")
 local character = script.Parent
 local player = players:GetPlayerFromCharacter(character)
 
-local hidersTeam = teams:WaitForChild("Hiders")
-local seekersTeam = teams:WaitForChild("Seekers")
+local rebelsTeam = teams:WaitForChild("Rebels")
+local huntersTeam = teams:WaitForChild("Hunters")
 local lobbyTeam = teams:WaitForChild("Lobby")
 
 local neon = character:WaitForChild("Neon")
 
 function changeNeon()
-	if player.Team == hidersTeam then
-		neon.Color = HIDERS_COLOR
-	elseif player.Team == seekersTeam then
-		neon.Color = SEEKERS_COLOR
+	if player.Team == rebelsTeam then
+		neon.Color = REBELS_COLOR
+	elseif player.Team == huntersTeam then
+		neon.Color = HUNTERS_COLOR
 	elseif player.Team == lobbyTeam then
 		neon.Color = LOBBY_COLOR
 	end
