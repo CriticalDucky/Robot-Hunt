146d8a1334057e991ef528b8f4abfaa20b6bcf7b
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-07 12:29:58 -0500

    Add action registration and handling

--- src/StarterPlayerScripts/Data/OperationManagement/External/RoundData.client.lua
+++ src/StarterPlayerScripts/Data/OperationManagement/External/RoundData.client.lua
@@ -3,16 +3,70 @@
 --#region Imports
 
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
 
 local replicatedStorageData = ReplicatedStorage:WaitForChild "Data"
 
 local ClientState = require(replicatedStorageData:WaitForChild "ClientState")
 local ClientServerCommunication = require(replicatedStorageData:WaitForChild "ClientServerCommunication")
+local Fusion = require(ReplicatedFirst:WaitForChild("Vendor"):WaitForChild "Fusion")
+
+local peek = Fusion.peek
 
 --#endregion
 
-ClientServerCommunication.registerActionAsync("UpdateRoundData", function(roundData)
-    ClientState.external.roundData:set(roundData)
+ClientServerCommunication.registerActionAsync(
+	"InitializeRoundData",
+	function(roundData) ClientState.external.roundData:set(roundData) end
+)
+
+ClientServerCommunication.registerActionAsync("UpdateRoundType", function(roundType)
+	local roundData = peek(ClientState.external.roundData)
+
+	if roundData then roundData.roundType = roundType end
+
+	ClientState.external.roundData:set(roundData)
 end)
 
-ClientServerCommunication.replicateAsync "UpdateRoundData"
+ClientServerCommunication.registerActionAsync("UpdatePhaseType", function(phaseType)
+	local roundData = peek(ClientState.external.roundData)
+
+	if roundData then roundData.phaseType = phaseType end
+
+	ClientState.external.roundData:set(roundData)
+end)
+
+ClientServerCommunication.registerActionAsync("UpdatePhaseEndTime", function(phaseEndTime)
+	local roundData = peek(ClientState.external.roundData)
+
+	if roundData then roundData.phaseEndTime = phaseEndTime end
+
+	ClientState.external.roundData:set(roundData)
+end)
+
+ClientServerCommunication.registerActionAsync("UpdatePlayerData", function(data)
+	local roundData = peek(ClientState.external.roundData)
+
+	local playerId = data.targetPlayerId
+	local playerData = data.playerData
+
+	if not roundData then return end
+
+	if playerId then
+		for i, currentPlayerData in ipairs(roundData.playerData) do
+			if not (currentPlayerData.playerId == playerId) then continue end
+
+			if data.data then
+				roundData.playerData[i] = data.data
+			else
+				table.remove(roundData.playerData, i)
+			end
+		end
+	elseif playerData then
+		roundData.playerData = playerData
+	end
+
+	ClientState.external.roundData:set(roundData)
+end)
+
+ClientServerCommunication.replicateAsync "InitializeRoundData"
