bc8da49f65748dc39e1027aa44a5d2634aa908b0
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-07 12:29:47 -0500

    Add separate replcation functions

--- src/ServerStorage/GameLoop/RoundDataManager.lua
+++ src/ServerStorage/GameLoop/RoundDataManager.lua
@@ -1,6 +1,6 @@
 --!strict
 
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
 local Players = game:GetService "Players"
 
@@ -8,87 +8,126 @@ local ClientServerCommunication = require(ReplicatedStorage.Data.ClientServerCom
 
 local Enums = require(ReplicatedFirst.Enums)
 
+type PlayerData = {
+	playerId: number,
+
+	-- True if the player is alive, false if they are dead
+	alive: boolean,
+
+	-- The player's current health (0-100)
+	health: number,
+}
+
 type RoundData = {
-    -- The current round type enum (Enums.RoundType)
-    currentRoundType: number?,
-    -- The current phase type enum (Enums.PhaseType)
-    currentPhaseType: number?,
-    -- The Unix timestamp of when the phase started
-    phaseStartTime: number?,
+	-- The current round type enum (Enums.RoundType)
+	currentRoundType: number?,
 
-    --[[
-        An array players in the round.
-        Round player data is not deleted when a player leaves the round, but alive is set to false.
-    ]]
-    publicPlayerData: {
-        {
-            playerId: number,
+	-- The current phase type enum (Enums.PhaseType)
+	currentPhaseType: number?,
 
-            -- True if the player is alive, false if they are dead
-            alive: boolean,
+	-- The Unix timestamp of when the phase should end
+	phaseEndTime: number?,
 
-            -- The player's current health (0-100)
-            health: number,
-        }?
-    },
-
-    --[[
-        An array of private player data.
-        Private player data is deleted when a player leaves the round.
-    ]]
-    privatePlayerData: {
-        {
-            playerId: number,
-
-            data: {
-                
-            },
-        }?
-    },
+	playerData: { PlayerData },
 }
 
 local roundData: RoundData = {
-    currentRoundType = nil,
-    currentPhaseType = Enums.PhaseType.NotEnoughPlayers,
-    phaseStartTime = nil,
+	currentRoundType = nil,
+	currentPhaseType = Enums.PhaseType.NotEnoughPlayers,
+	phaseEndTime = nil,
 
-    publicPlayerData = {},
-    privatePlayerData = {},
+	playerData = {},
 }
 
 local RoundDataManager = {}
 
 RoundDataManager.data = roundData
 
+function RoundDataManager.filterPlayerData(playerData: PlayerData, player: Player)
+	return {
+		playerId = playerData.playerId,
+		alive = playerData.alive,
+		health = playerData.health,
+	}
+end
+
 --[[
     Retrieves the round data and returns a filtered version of it for the client.
 ]]
 function RoundDataManager.getFilteredData(player: Player)
-    local filteredData = {}
+	local filteredData = {}
 
-    filteredData.currentRoundType = roundData.currentRoundType
-    filteredData.currentPhaseType = roundData.currentPhaseType
-    filteredData.phaseStartTime = roundData.phaseStartTime
-    filteredData.publicPlayerData = roundData.publicPlayerData
+	filteredData.currentRoundType = roundData.currentRoundType
+	filteredData.currentPhaseType = roundData.currentPhaseType
+	filteredData.phaseEndTime = roundData.phaseEndTime
+	
+    filteredData.playerData = {}
 
-    for _, playerData in ipairs(roundData.privatePlayerData) do
-        assert(playerData)
+    for _, playerData in ipairs(roundData.playerData) do
+        table.insert(filteredData.playerData, RoundDataManager.filterPlayerData(playerData, player))
+    end
 
-        if playerData.playerId == player.UserId then
-            filteredData.privatePlayerData = playerData.data
+	return filteredData
+end
 
-            break
+function RoundDataManager.initializedRoundDataAsync(player: Player?)
+	local players = if player then { player } else Players:GetPlayers()
+
+	for _, player in ipairs(players) do
+		ClientServerCommunication.replicateAsync("InitializeRoundData", RoundDataManager.getFilteredData(player), player)
+	end
+end
+
+function RoundDataManager.replicateRoundTypeAsync()
+	ClientServerCommunication.replicateAsync("UpdateRoundType", roundData.currentRoundType)
+end
+
+function RoundDataManager.replicatePhaseTypeAsync()
+	ClientServerCommunication.replicateAsync("UpdatePhaseType", roundData.currentPhaseType)
+end
+
+function RoundDataManager.replicatePhaseEndTimeAsync()
+	ClientServerCommunication.replicateAsync("UpdatePhaseEndTime", roundData.phaseEndTime)
+end
+
+--[[
+    Replicates round player data to all clients.
+
+    If targetPlayer is provided, only replicate the data for that player.
+]]
+function RoundDataManager.replicatePlayerDataAsync(targetPlayer: Player?)
+    local playerData: PlayerData?
+
+    if targetPlayer then
+        for _, data in ipairs(roundData.playerData) do
+            if data.playerId == targetPlayer.UserId then
+                playerData = data
+                break
+            end
+        end
+
+        if playerData then
+            playerData = RoundDataManager.filterPlayerData(playerData, targetPlayer)
         end
     end
 
-    return filteredData
-end
+    local data = {
+        playerData = playerData or {},
+        targetPlayerId = targetPlayer and targetPlayer.UserId or nil,
+    }
 
-function RoundDataManager.replicateDataAsync(player: Player?)
-    local players = if player then { player } else Players:GetPlayers()
+    for _, player in ipairs(Players:GetPlayers()) do
+        if not targetPlayer then
+            local filteredPlayerDatas = {}
 
-    for _, player in ipairs(players) do
-        ClientServerCommunication.replicateAsync("UpdateRoundData", RoundDataManager.getFilteredData(player), player)
+            for _, newData in ipairs(roundData.playerData) do
+                table.insert(filteredPlayerDatas, RoundDataManager.filterPlayerData(newData, player))
+            end
+
+            data.playerData = filteredPlayerDatas
+        end
+
+        ClientServerCommunication.replicateAsync("UpdatePlayerData", data, player)
     end
 end
 
