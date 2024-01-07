e2fad1c3afa4c7b9bbd3558ba12a0e8b903cf08b
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-06 22:32:07 -0500

    Fix player collision

--- src/StarterCharacterScripts/Collision.server.lua
+++ src/StarterCharacterScripts/Collision.server.lua
@@ -2,10 +2,13 @@ local PhysicsService = game:GetService("PhysicsService")
 
 local character = script.Parent
 
+PhysicsService:RegisterCollisionGroup("Character")
+PhysicsService:CollisionGroupSetCollidable("Character", "Character", false)
+
 for _, v in ipairs(character:GetDescendants()) do
 	if v:IsA("BasePart") then
 		v.CollisionGroup = "Character"
 	end
 end
 
-PhysicsService:CollisionGroupSetCollidable("Character", "Character", false)
+
