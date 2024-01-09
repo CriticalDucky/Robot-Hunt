9ecb91ae5a31c07c6061a322a76dcb7fd2bcae46
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-08 19:16:11 -0500

    Begin adding real functionality

--- src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
+++ src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
@@ -5,15 +5,18 @@ local GameLoop = ServerStorage.GameLoop
 
 local Enums = require(ReplicatedFirst.Enums)
 local Modules = require(GameLoop.Modules)
--- local Actions = require(GameLoop.Actions)
+local Actions = require(GameLoop.Actions)
 local RoundDataManager = require(GameLoop.RoundDataManager)
 
 local DefaultRound = {}
 
 function DefaultRound.begin()
+	local playingPlayers = Actions.getEligiblePlayers()
+	local sorted = Actions.sortPlayers(playingPlayers)
+
 	RoundDataManager.data.currentRoundType = Enums.RoundType.defaultRound
 
-	return Modules.DefaultRound.Hiding
+	return Modules.DefaultRound.Infiltration
 		.begin()
 		:andThenCall(Modules.DefaultRound.PhaseOne.begin)
         :andThenCall(Modules.DefaultRound.PhaseTwo.begin)
