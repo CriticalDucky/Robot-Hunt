179da4cf0eba48091a10fa6ff0464c1be59fc6ff
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-08 19:16:51 -0500

    Add humanoid property manager

---
+++ src/StarterPlayerScripts/Robot/HumanoidPropertyManager.client.lua
@@ -0,0 +1,52 @@
+local NORMAL_SPEED = 16
+local SPEED_BOOST = 3
+local CRAWL_SPEED = 7
+
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local Players = game:GetService "Players"
+
+local ClientState = require(ReplicatedStorage:WaitForChild("Data"):WaitForChild "ClientState")
+
+local Fusion = require(ReplicatedFirst:WaitForChild("Vendor"):WaitForChild "Fusion")
+local Hydrate = Fusion.Hydrate
+local Computed = Fusion.Computed
+local Spring = Fusion.Spring
+
+local isCrawling = ClientState.actions.isCrawling
+
+local player = Players.LocalPlayer
+
+local speedBoost = Computed(function()
+    return false -- TODO: update this with the client state
+end)
+
+local cameraOffset = Spring(Computed(function(Use)
+    if Use(isCrawling) then
+        return Vector3.new(0, -3, 0)
+    else
+        return Vector3.new(0, 0, 0)
+    end
+end), 25, 1)
+
+local function onCharacterAdded(character)
+    local humanoid = character:WaitForChild("Humanoid")
+
+    Hydrate(humanoid) {
+        WalkSpeed = Computed(function(use)
+            local isCrawling = use(isCrawling)
+            local isSpeedBoosted = use(speedBoost)
+    
+            local base_speed = if isCrawling then CRAWL_SPEED else NORMAL_SPEED
+    
+            return base_speed + if isSpeedBoosted then SPEED_BOOST else 0
+        end),
+        CameraOffset = cameraOffset
+    }
+end
+
+player.CharacterAdded:Connect(onCharacterAdded)
+
+if player.Character then
+    onCharacterAdded(player.Character)
+end
