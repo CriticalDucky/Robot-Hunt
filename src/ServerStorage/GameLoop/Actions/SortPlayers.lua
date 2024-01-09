410da7618c5d117e872932ff83eb4575da995feb
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-08 19:12:55 -0500

    Add sort player action

---
+++ src/ServerStorage/GameLoop/Actions/SortPlayers.lua
@@ -0,0 +1,34 @@
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+
+local RoundConfiguration = require(ReplicatedStorage.Configuration.RoundConfiguration)
+local Table = require(ReplicatedFirst.Utility.Table)
+local Enums = require(ReplicatedFirst.Enums)
+
+hunterToRebelRatio = RoundConfiguration.hunterToRebelRatio
+
+--[[
+    Sorts the players into teams based on the hunterToRebelRatio
+    @param players The players to sort
+    @return A table containing the sorted players
+]]
+return function(players)
+	assert(#players >= 2, "There must be at least 2 players to start a round")
+
+    local sorted = {
+        [Enums.TeamType.rebels] = {}
+    }
+
+	local numHunters = math.ceil(#players * hunterToRebelRatio)
+	local hunters = Table.randomArraySelection(players, numHunters)
+
+    sorted[Enums.TeamType.hunters] = hunters
+
+    for _, player in ipairs(players) do
+        if not table.find(hunters, player) then
+            table.insert(sorted[Enums.TeamType.rebels], player)
+        end
+    end
+
+    return sorted
+end
