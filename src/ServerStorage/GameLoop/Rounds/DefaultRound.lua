79d954da3a60e3cebc9afe6fc48de1b6156b8676
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-23 08:54:07 -0500

    Add to script

--- src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
+++ src/ServerStorage/GameLoop/Rounds/DefaultRound.lua
@@ -1,6 +1,8 @@
 local ServerStorage = game:GetService "ServerStorage"
 local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
 local Teams = game:GetService "Teams"
+local Players = game:GetService "Players"
 
 local GameLoop = ServerStorage.GameLoop
 
@@ -8,10 +10,11 @@ local Enums = require(ReplicatedFirst.Enums)
 local Modules = require(GameLoop.Modules)
 local Actions = require(GameLoop.Actions)
 local RoundDataManager = require(GameLoop.RoundDataManager)
+local RoundConfiguration = require(ReplicatedStorage.Configuration.RoundConfiguration)
 
 local teams = {
-	[Enums.TeamType.rebels] = Teams:WaitForChild("Rebels"),
-	[Enums.TeamType.hunters] = Teams:WaitForChild("Hunters"),
+	[Enums.TeamType.rebels] = Teams:WaitForChild "Rebels",
+	[Enums.TeamType.hunters] = Teams:WaitForChild "Hunters",
 }
 
 local DefaultRound = {}
@@ -22,7 +25,7 @@ function DefaultRound.begin()
 
 	local playerDatas = {}
 
-	for teamType, players: {Player} in pairs(sorted) do
+	for teamType, players: { Player } in pairs(sorted) do
 		for _, player in players do
 			player.Team = teams[teamType]
 
@@ -32,11 +35,36 @@ function DefaultRound.begin()
 		end
 	end
 
+	local numRequiredTerminals
+	do
+		local numPlayers = #sorted[Enums.TeamType.rebels]
+
+		numRequiredTerminals = math.clamp(numPlayers, RoundConfiguration.minTerminals, RoundConfiguration.maxTerminals)
+	end
+
+	local totalTerminals = numRequiredTerminals + RoundConfiguration.extraTerminals
+	local batteryPercentage = math.random(
+		RoundConfiguration.batteryLowerPercentage * 1000,
+		RoundConfiguration.batteryUpperPercentage * 1000
+	) / 1000
+
 	return Modules.DefaultRound.Loading
-		.begin()
+		.begin({
+			numTerminals = totalTerminals,
+			batteryPercentage = batteryPercentage,
+		})
+		:andThen(function()
+			-- At this point the map should be ready for players to spawn in
+
+			for playerId, _ in pairs(playerDatas) do
+				local player = Players:GetPlayerByUserId(playerId)
+
+				if player then player:LoadCharacter() end
+			end
+		end)
 		:andThenCall(Modules.DefaultRound.Infiltration.begin)
 		:andThenCall(Modules.DefaultRound.PhaseOne.begin)
-        :andThenCall(Modules.DefaultRound.PhaseTwo.begin)
+		:andThenCall(Modules.DefaultRound.PhaseTwo.begin)
 end
 
 return DefaultRound
