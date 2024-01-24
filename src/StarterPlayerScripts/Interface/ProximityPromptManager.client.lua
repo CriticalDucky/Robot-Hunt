117f1065ce7b60d79145ab93f34dae128344a89a
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-24 16:44:27 -0500

    Add manager for proximity prompts

---
+++ src/StarterPlayerScripts/Interface/ProximityPromptManager.client.lua
@@ -0,0 +1,46 @@
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local Players = game:GetService "Players"
+
+local ClientState = require(ReplicatedStorage:WaitForChild("Data"):WaitForChild "ClientState")
+
+local Fusion = require(ReplicatedFirst:WaitForChild("Vendor"):WaitForChild "Fusion")
+
+local Hydrate = Fusion.Hydrate
+local Computed = Fusion.Computed
+
+local player = Players.LocalPlayer
+
+workspace.DescendantAdded:Connect(function(descendant)
+	if descendant:IsA "ProximityPrompt" then
+		if descendant.Name == "Button" then
+			Hydrate(descendant) {
+				Enabled = Computed(function(use)
+					local playerData = use(ClientState.external.roundData.playerData)[player.UserId]
+
+					if not playerData then return false end
+
+					local isCrawling = use(ClientState.actions.isCrawling)
+					local isShooting = playerData.actions.isShooting
+					local isHacking = playerData.actions.isHacking
+
+					return not isCrawling and not isShooting and not isHacking
+				end),
+			}
+		elseif descendant.Name == "Terminal" then
+            Hydrate(descendant) {
+                Enabled = Computed(function(use)
+                    local playerData = use(ClientState.external.roundData.playerData)[player.UserId]
+
+                    if not playerData then return false end
+
+                    local isCrawling = use(ClientState.actions.isCrawling)
+                    local isShooting = playerData.actions.isShooting
+                    local isHacking = playerData.actions.isHacking
+
+                    return not isCrawling and not isShooting and not isHacking
+                end),
+            }
+        end
+	end
+end)
