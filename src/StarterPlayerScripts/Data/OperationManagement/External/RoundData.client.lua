55607bbb13eca7424b2f1af6ab88ebe2ce7f43bd
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-24 20:30:19 -0500

    Add client handling

--- src/StarterPlayerScripts/Data/OperationManagement/External/RoundData.client.lua
+++ src/StarterPlayerScripts/Data/OperationManagement/External/RoundData.client.lua
@@ -10,63 +10,384 @@ local replicatedStorageData = ReplicatedStorage:WaitForChild "Data"
 local ClientState = require(replicatedStorageData:WaitForChild "ClientState")
 local ClientServerCommunication = require(replicatedStorageData:WaitForChild "ClientServerCommunication")
 local Fusion = require(ReplicatedFirst:WaitForChild("Vendor"):WaitForChild "Fusion")
+local Enums = require(ReplicatedFirst:WaitForChild "Enums")
 
 local peek = Fusion.peek
+local roundData = ClientState.external.roundData
+local PhaseType = Enums.PhaseType
 
 --#endregion
 
-ClientServerCommunication.registerActionAsync(
-	"InitializeRoundData",
-	function(roundData) ClientState.external.roundData:set(roundData) end
-)
-
-ClientServerCommunication.registerActionAsync("UpdateRoundType", function(roundType)
-	local roundData = peek(ClientState.external.roundData)
-
-	if roundData then roundData.roundType = roundType end
-
-	ClientState.external.roundData:set(roundData)
+ClientServerCommunication.registerActionAsync("InitializeRoundData", function(roundData)
+	for key, value in roundData do
+		roundData[key]:set(value)
+	end
 end)
 
-ClientServerCommunication.registerActionAsync("UpdatePhaseType", function(phaseType)
-	local roundData = peek(ClientState.external.roundData)
+ClientServerCommunication.registerActionAsync("SetPhase", function(data)
+	local phaseType = data.phaseType
+	local phaseEndTime = data.phaseEndTime
 
-	if roundData then roundData.phaseType = phaseType end
+	roundData.currentPhaseType:set(phaseType)
+	roundData.phaseEndTime:set(phaseEndTime)
 
-	ClientState.external.roundData:set(roundData)
+	if phaseType == PhaseType.Intermission then
+		roundData.playerData:set {}
+	elseif phaseType == PhaseType.Loading then
+		roundData.currentRoundType:set(nil)
+	end
 end)
 
-ClientServerCommunication.registerActionAsync("UpdatePhaseEndTime", function(phaseEndTime)
-	local roundData = peek(ClientState.external.roundData)
+--[[ How its like on the server
+	function RoundDataManager.addVictim(attacker: Player, victim: Player)
+	local attackerData = roundData.playerData[attacker.UserId]
+	local victimData = roundData.playerData[victim.UserId]
 
-	if roundData then roundData.phaseEndTime = phaseEndTime end
+	assert(attackerData, "Attacker data does not exist")
+	assert(victimData, "Victim data does not exist")
 
-	ClientState.external.roundData:set(roundData)
-end)
+	victimData.lastAttackerId = attacker.UserId
+	attackerData.victims[attacker.UserId] = true
 
-ClientServerCommunication.registerActionAsync("UpdatePlayerData", function(data)
-	local roundData = peek(ClientState.external.roundData)
+	ClientServerCommunication.replicateAsync("UpdateVictims", {
+		attackerId = attacker.UserId,
+		victims = attackerData.victims,
+	})
 
-	local playerId = data.targetPlayerId
-	local playerData = data.playerData
+	onDataUpdatedEvent:Fire(roundData)
+end
 
-	if not roundData then return end
+function RoundDataManager.removeVictim(attacker: Player, victim: Player)
+	local attackerData = roundData.playerData[attacker.UserId]
+	local victimData = roundData.playerData[victim.UserId]
 
-	if playerId then
-		for i, currentPlayerData in ipairs(roundData.playerData) do
-			if not (currentPlayerData.playerId == playerId) then continue end
+	assert(attackerData, "Attacker data does not exist")
+	assert(victimData, "Victim data does not exist")
 
-			if data.data then
-				roundData.playerData[i] = data.data
-			else
-				table.remove(roundData.playerData, i)
-			end
-		end
-	elseif playerData then
-		roundData.playerData = playerData
+	attackerData.victims[victim.UserId] = nil
+
+	ClientServerCommunication.replicateAsync("UpdateVictims", {
+		attackerId = attacker.UserId,
+		victims = attackerData.victims,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("UpdateVictims", function(data)
+	local attackerId = data.attackerId
+	local victims = data.victims
+
+	local newPlayerData = peek(roundData.playerData)
+	local attackerData = newPlayerData[attackerId]
+
+	if not newPlayerData or not attackerData then
+		return
 	end
 
-	ClientState.external.roundData:set(roundData)
+	attackerData.victims = victims
+
+	roundData.playerData:set(newPlayerData)
 end)
 
-ClientServerCommunication.replicateAsync "InitializeRoundData"
+--[[ How its like on the server
+	function RoundDataManager.killPlayer(victim: Player, killer: Player?)
+	local playerData = roundData.playerData[victim.UserId]
+
+	assert(playerData, "Player data does not exist")
+
+	playerData.status = Enums.PlayerStatus.dead
+	playerData.health = 0
+	playerData.armor = 0
+	playerData.lifeSupport = 0
+
+	if killer then
+		playerData.killedById = killer and killer.UserId or nil
+
+		local killerData = roundData.playerData[killer.UserId]
+
+		killerData.stats.kills += 1
+	end
+
+	ClientServerCommunication.replicateAsync("KillPlayer", {
+		victimId = victim.UserId,
+		killedById = playerData.killedById,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("KillPlayer", function(data)
+	local victimId = data.victimId
+	local killedById = data.killedById
+
+	local newPlayerData = peek(roundData.playerData)
+	local victimData = newPlayerData[victimId]
+
+	if not newPlayerData or not victimData then
+		return
+	end
+
+	victimData.status = Enums.PlayerStatus.dead
+	victimData.health = 0
+	victimData.armor = 0
+	victimData.lifeSupport = 0
+	victimData.killedById = killedById
+
+	if killedById then
+		local killerData = newPlayerData[killedById]
+
+		if killerData then
+			killerData.stats.kills += 1
+		end
+	end
+
+	roundData.playerData:set(newPlayerData)
+end)
+
+--[[
+	function RoundDataManager.revivePlayer(player: Player)
+	local playerData = roundData.playerData[player.UserId]
+
+	assert(playerData, "Player data does not exist")
+
+	playerData.status = Enums.PlayerStatus.alive
+	playerData.health = 100
+	playerData.armor = 0
+	playerData.lifeSupport = 100
+
+	ClientServerCommunication.replicateAsync("revivePlayer", {
+		playerId = player.UserId,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("RevivePlayer", function(data)
+	local playerId = data.playerId
+
+	local newPlayerData = peek(roundData.playerData)
+	local playerData = newPlayerData[playerId]
+
+	if not newPlayerData or not playerData then
+		return
+	end
+
+	playerData.status = Enums.PlayerStatus.alive
+	playerData.health = 100
+	playerData.armor = 0
+	playerData.lifeSupport = 100
+
+	roundData.playerData:set(newPlayerData)
+end)
+
+--[[
+	function RoundDataManager.setHealth(player: Player, armor: number?, health: number?)
+	local playerData = roundData.playerData[player.UserId]
+
+	assert(playerData, "Player data does not exist")
+
+	if armor then
+		playerData.armor = armor
+	end
+
+	if health then
+		playerData.health = health
+
+		if health <= 0 then
+			playerData.status = Enums.PlayerStatus.lifeSupport
+		end
+	end
+
+	ClientServerCommunication.replicateAsync("UpdateHealth", {
+		playerId = player.UserId,
+		health = playerData.health,
+		armor = playerData.armor,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("UpdateHealth", function(data)
+	local playerId = data.playerId
+	local health = data.health
+	local armor = data.armor
+
+	local newPlayerData = peek(roundData.playerData)
+	local playerData = newPlayerData[playerId]
+
+	if not newPlayerData or not playerData then
+		return
+	end
+
+	if armor then
+		playerData.armor = armor
+	end
+
+	if health then
+		playerData.health = health
+
+		if health <= 0 then
+			playerData.status = Enums.PlayerStatus.lifeSupport
+		end
+	end
+
+	roundData.playerData:set(newPlayerData)
+end)
+
+--[[
+	function RoundDataManager.setLifeSupport(player: Player, lifeSupport: number)
+	local playerData = roundData.playerData[player.UserId]
+
+	assert(lifeSupport >= 0 and lifeSupport <= 100, "Life support must be between 0 and 100")
+
+	playerData.lifeSupport = lifeSupport
+
+	if lifeSupport <= 0 then
+		return RoundDataManager.killPlayer(player) -- u had a good run
+	end
+
+	ClientServerCommunication.replicateAsync("UpdateLifeSupport", {
+		playerId = player.UserId,
+		lifeSupport = lifeSupport,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+
+function RoundDataManager.incrementLifeSupport(player: Player, amount: number)
+	local playerData = roundData.playerData[player.UserId]
+
+	assert(playerData, "Player data does not exist")
+
+	local lifeSupport = math.clamp(playerData.lifeSupport + amount, 0, 100)
+
+	RoundDataManager.setLifeSupport(player, lifeSupport)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("UpdateLifeSupport", function(data)
+	local playerId = data.playerId
+	local lifeSupport = data.lifeSupport
+
+	local newPlayerData = peek(roundData.playerData)
+	local playerData = newPlayerData[playerId]
+
+	if not newPlayerData or not playerData then
+		return
+	end
+
+	playerData.lifeSupport = lifeSupport
+
+	roundData.playerData:set(newPlayerData)
+end)
+
+--[[
+	function RoundDataManager.setAmmo(player: Player, ammo: number)
+	local playerData = roundData.playerData[player.UserId]
+
+	assert(ammo >= 0 and ammo <= 100, "Ammo must be between 0 and 100")
+	assert(playerData, "Player data does not exist")
+
+	playerData.ammo = ammo
+
+	ClientServerCommunication.replicateAsync("UpdateAmmo", {
+		playerId = player.UserId,
+		ammo = ammo,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+
+function RoundDataManager.incrementAmmo(player: Player, amount: number)
+	local playerData = roundData.playerData[player.UserId]
+
+	assert(playerData, "Player data does not exist")
+	assert(playerData.ammo, "Player data ammo does not exist")
+
+	amount = math.clamp(playerData.ammo + amount, 0, 100)
+
+	RoundDataManager.setAmmo(player, amount)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("UpdateAmmo", function(data)
+	local playerId = data.playerId
+	local ammo = data.ammo
+
+	local newPlayerData = peek(roundData.playerData)
+	local playerData = newPlayerData[playerId]
+
+	if not newPlayerData or not playerData then
+		return
+	end
+
+	playerData.ammo = ammo
+
+	roundData.playerData:set(newPlayerData)
+end)
+
+--[[
+	function RoundDataManager.updateBatteryHolder(batteryId: number, holder: Player?)
+	local batteryData = roundData.batteryData[batteryId]
+
+	assert(batteryData, "Battery data does not exist")
+
+	batteryData.holder = holder and holder.UserId or nil
+
+	ClientServerCommunication.replicateAsync("updateBatteryHolder", {
+		batteryId = batteryId,
+		holderId = batteryData.holder,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("UpdateBatteryHolder", function(data)
+	local batteryId = data.batteryId
+	local holderId = data.holderId
+
+	local newBatteryData = peek(roundData.batteryData)
+	
+	for _, batteryData in pairs(newBatteryData) do
+		if batteryData.id == batteryId then
+			batteryData.holder = holderId
+			roundData.batteryData:set(newBatteryData)
+		end
+	end
+end)
+
+--[[
+	function RoundDataManager.setUpRound(
+	roundType: number,
+	playerDatas: { [number]: RoundPlayerData },
+	terminalData: { RoundTerminalData },
+	batteryData: { RoundBatteryData }
+)
+	roundData.currentRoundType = roundType
+	roundData.playerData = playerDatas
+
+	roundData.terminalData = terminalData
+	roundData.batteryData = batteryData
+
+	ClientServerCommunication.replicateAsync("SetUpRound", {
+		roundType = roundType,
+		playerData = playerDatas,
+		terminalData = terminalData,
+	})
+
+	onDataUpdatedEvent:Fire(roundData)
+end
+]]
+
+ClientServerCommunication.registerActionAsync("SetUpRound", function(data)
+	roundData.currentRoundType:set(data.roundType)
+	roundData.playerData:set(data.playerData)
+	roundData.terminalData:set(data.terminalData)
+	roundData.batteryData:set(data.batteryData)
+end)
