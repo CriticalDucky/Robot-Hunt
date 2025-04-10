c16ad65e3de2cea439fbf1374f9cff54e13b0b2d
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-04-10 10:07:44 -0400

    Add logic

--- src/ServerScriptService/GameLoop/TerminalManager.server.lua
+++ src/ServerScriptService/GameLoop/TerminalManager.server.lua
@@ -19,10 +19,62 @@ local Enums = require(ReplicatedFirst.Enums)
 
 type RoundPlayerData = Types.RoundPlayerData
 
-ProximityPromptService.PromptTriggered:Connect(function(prompt, player)
-	if not RoundConfiguration.roundPhases[RoundDataManager.data.currentPhaseType] then return end
+function getRandomCooldownTime()
+	return math.random(RoundConfiguration.minTerminalCooldown, RoundConfiguration.maxTerminalCooldown)
+end
 
+function onClientPuzzleResult(player, terminalId, success)
+	local terminalData = RoundDataManager.data.terminalData[terminalId]
+
+	assert(terminalData, "Terminal data not found")
+
+	-- remove from puzzle queue
+	local index = table.find(terminalData.puzzleQueue, player)
+	if index then
+		table.remove(terminalData.puzzleQueue, index)
+	end
+
+	if success then
+		RoundDataManager.incrementTerminalProgress(terminalId, RoundConfiguration.puzzleBonusPerPlayer)
+	else
+		RoundDataManager.incrementTerminalProgress(terminalId, -RoundConfiguration.puzzlePenaltyPerPlayer)
+		RoundDataManager.removeHacker(terminalId, player.UserId) -- haha
+
+		RoundDataManager.setTerminalStates(terminalId, {
+			isErrored = true,
+		})
+	end
+
+	if #terminalData.puzzleQueue == 0 then
+		RoundDataManager.setTerminalStates(terminalId, {
+			isPuzzleMode = false,
+		})
+	end
+end
+
+function promptPuzzle(terminalData: Types.RoundTerminalData)
+	RoundDataManager.setTerminalStates(terminalData.id, {
+		isPuzzleMode = true,
+	})
+
+	terminalData.cooldown = getRandomCooldownTime()
+
+	for _, player in ipairs(terminalData.hackers) do
+		table.insert(terminalData.puzzleQueue, player)
+		ClientServerCommunication.replicateAsync("PromptTerminalPuzzle", player)
+
+		task.delay(RoundConfiguration.puzzleTimeout, function()
+			if table.find(terminalData.puzzleQueue, player) then
+				onClientPuzzleResult(player, terminalData.id, false)
+			end
+		end)
+	end
+end
+
+ProximityPromptService.PromptTriggered:Connect(function(prompt, player)
 	if prompt.Name ~= "Terminal" then return end
+
+	if not RoundConfiguration.roundPhases[RoundDataManager.data.currentPhaseType] then return end
 
 	local terminalData
 	local terminalModel = prompt:FindFirstAncestor "Terminal"
@@ -42,27 +94,48 @@ ProximityPromptService.PromptTriggered:Connect(function(prompt, player)
 	if terminalData.progress >= 100 then return end
 
 	for _, data in pairs(RoundDataManager.data.terminalData) do
-		if data.hackers[player.UserId] then return end
+		if table.find(data.hackers, player) then return end
 	end
 
 	for _, data in pairs(RoundDataManager.data.batteryData) do
 		if data.holder == player.UserId then return end
 	end
 
-    local playerData = RoundDataManager.data.playerData[player.UserId]
+	local playerData = RoundDataManager.data.playerData[player.UserId]
 
-    if not playerData then return end
-    if playerData.status ~= Enums.PlayerStatus.alive then return end
-    if playerData.actions.isHacking then return end
-    if playerData.actions.isShooting then return end
+	if not playerData then return end
+	if playerData.status ~= Enums.PlayerStatus.alive then return end
+	if playerData.actions.isHacking then return end
+	if playerData.actions.isShooting then return end
 
-    
+	RoundDataManager.addHacker(terminalData.id, player.UserId)
 end)
 
 RunService.Heartbeat:Connect(function(dt)
-    for _, terminalData in pairs(RoundDataManager.data.terminalData) do
-        if terminalData.progress >= 100 then continue end
+	for _, terminalData in pairs(RoundDataManager.data.terminalData) do
+		if terminalData.progress >= 100 then continue end
 
-        
-    end
+		local numHackers = #terminalData.hackers
+		if terminalData.isPuzzleMode or numHackers == 0 then continue end
+
+		if terminalData.progress == 0 then
+			terminalData.cooldown = getRandomCooldownTime()
+		end
+
+		RoundDataManager.incrementTerminalProgress(
+			terminalData.id,
+			dt * RoundConfiguration.terminalProgressPerSecondPerPlayer * numHackers
+		)
+
+		if terminalData.progress >= 100 then continue end
+
+		terminalData.cooldown -= dt
+		if terminalData.cooldown <= 0 then
+			if terminalData.progress >= 97 then
+				terminalData.cooldown = 10000 -- we don't want to trigger a puzzle if the terminal is almost done
+			else
+				promptPuzzle(terminalData)
+			end
+		end
+	end
 end)
