fb971bbac677e2a31cda02ecadf56480c92f18cd
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-04-16 09:43:08 -0400

    Remove localized collision managmeent

--- src/StarterCharacterScripts/Collision.server.lua
+++ src/StarterCharacterScripts/Collision.server.lua
@@ -1,9 +1,4 @@
-local PhysicsService = game:GetService("PhysicsService")
-
 local character = script.Parent
-
-PhysicsService:RegisterCollisionGroup("Character")
-PhysicsService:CollisionGroupSetCollidable("Character", "Character", false)
 
 for _, v in ipairs(character:GetDescendants()) do
 	if v:IsA("BasePart") then
