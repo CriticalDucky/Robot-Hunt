6847c8e5bec534b339f19bc8ae362d35696d76d6
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-04-23 02:20:46 -0400

    Finish terminal manager

--- src/ServerScriptService/GameLoop/TerminalManager.server.lua
+++ src/ServerScriptService/GameLoop/TerminalManager.server.lua
@@ -36,7 +36,7 @@ function onClientPuzzleResult(player, terminalId, success)
 		RoundDataManager.incrementTerminalProgress(terminalId, RoundConfiguration.puzzleBonusPerPlayer)
 	else
 		RoundDataManager.incrementTerminalProgress(terminalId, -RoundConfiguration.puzzlePenaltyPerPlayer)
-		RoundDataManager.removeHacker(terminalId, player.UserId) -- haha
+		RoundDataManager.removeHackers(terminalId, player) -- haha
 
 		RoundDataManager.setTerminalStates(terminalId, {
 			isErrored = true,
@@ -51,13 +51,15 @@ function onClientPuzzleResult(player, terminalId, success)
 end
 
 function promptPuzzle(terminalData: Types.RoundTerminalData)
+	warn("PUZZLE PROMPTED!!!!")
+
 	RoundDataManager.setTerminalStates(terminalData.id, {
 		isPuzzleMode = true,
 	})
 
 	terminalData.cooldown = getRandomCooldownTime()
 
-	for _, player in ipairs(terminalData.hackers) do
+	for _, player in pairs(terminalData.hackers) do
 		table.insert(terminalData.puzzleQueue, player)
 		ClientServerCommunication.replicateAsync("PromptTerminalPuzzle", player)
 
@@ -70,46 +72,81 @@ function promptPuzzle(terminalData: Types.RoundTerminalData)
 end
 
 ProximityPromptService.PromptTriggered:Connect(function(prompt, player)
-	if prompt.Name ~= "Terminal" then return end
+	if prompt.Name ~= "Terminal" then 
+		print("Prompt is not a Terminal.")
+		return 
+	end
 
-	if not RoundConfiguration.roundPhases[RoundDataManager.data.currentPhaseType] then return end
+	if not RoundConfiguration.terminalPhases[RoundDataManager.data.currentPhaseType] then 
+		print("Current phase type does not allow terminal interaction.")
+		return 
+	end
 
 	local terminalData
 	local terminalModel = prompt:FindFirstAncestor "Terminal"
 
-	if not terminalModel then return end
+	if not terminalModel then 
+		print("Terminal model not found.")
+		return 
+	end
 
 	for _, data in pairs(RoundDataManager.data.terminalData) do
 		if data.model == terminalModel then
 			terminalData = data
-
 			break
 		end
 	end
 
-	if not terminalData then return end
+	if not terminalData then 
+		print("Terminal data not found.")
+		return 
+	end
 
-	if terminalData.progress >= 100 then return end
+	if terminalData.progress >= 100 then 
+		print("Terminal progress is already complete.")
+		return 
+	end
 
 	for _, data in pairs(RoundDataManager.data.terminalData) do
-		if table.find(data.hackers, player) then return end
+		if table.find(data.hackers, player) then 
+			print("Player is already hacking another terminal.")
+			return 
+		end
 	end
 
 	for _, data in pairs(RoundDataManager.data.batteryData) do
-		if data.holder == player.UserId then return end
+		if data.holder == player.UserId then 
+			print("Player is holding a battery and cannot hack.")
+			return 
+		end
 	end
 
 	local playerData = RoundDataManager.data.playerData[player.UserId]
 
-	if not playerData then return end
-	if playerData.status ~= Enums.PlayerStatus.alive then return end
-	if playerData.actions.isHacking then return end
-	if playerData.actions.isShooting then return end
+	if not playerData then 
+		print("Player data not found.")
+		return 
+	end
+	if playerData.status ~= Enums.PlayerStatus.alive then 
+		print("Player is not alive and cannot hack.")
+		return 
+	end
+	if playerData.actions.isHacking then 
+		print("Player is already hacking.")
+		return 
+	end
+	if playerData.actions.isShooting then 
+		print("Player is shooting and cannot hack.")
+		return 
+	end
 
 	local seat = prompt.Parent.Parent:FindFirstChild "Seat" :: BasePart
 	local character = player.Character
 
-	if not seat or not character then return end
+	if not seat or not character then 
+		print("Seat or character not found.")
+		return 
+	end
 
 	local characterPivot = character:GetPivot()
 	local seatPivot = seat:GetPivot()
@@ -117,12 +154,14 @@ ProximityPromptService.PromptTriggered:Connect(function(prompt, player)
 	-- Set the character's x and z position to the seat's x and z position
 	local destinationCFrame = CFrame.new(seatPivot.Position.X, characterPivot.Position.Y, seatPivot.Position.Z)
 
-	-- Set the character's rotation to the seat's rotation
+
 	destinationCFrame *= CFrame.Angles(0, seatPivot.Rotation.Y, 0)
+	-- but we need to offset the rotation by 90 degrees to the right for some reason
+	destinationCFrame *= CFrame.Angles(0, math.rad(90), 0)
 
 	character:PivotTo(destinationCFrame)
 
-	RoundDataManager.addHacker(terminalData.id, player.UserId)
+	RoundDataManager.addHacker(terminalData.id, player)
 end)
 
 RunService.Heartbeat:Connect(function(dt)
@@ -134,25 +173,30 @@ RunService.Heartbeat:Connect(function(dt)
 		if numHackers == 0 then continue end
 
 		do
+			local safeHackers = {}
+
 			local zone = terminalData.model:FindFirstChild "Zone"
 
 			local parts = workspace:GetPartBoundsInBox(zone.CFrame, zone.Size)
 
-			for hacker in table.unpack(terminalData.hackers) do
-				local player = Players:GetPlayerByUserId(hacker)
-
-				if not player then RoundDataManager.removeHacker(terminalData.id, hacker) end
-
-				local character = player.Character
+			for _, hacker in pairs(terminalData.hackers) do
+				local character = hacker.Character
 
 				if not character then
-					RoundDataManager.removeHacker(terminalData.id, hacker)
+					RoundDataManager.removeHackers(terminalData.id, hacker)
 				else
 					for _, part in ipairs(parts) do
 						if part:IsDescendantOf(character) then
-							RoundDataManager.removeHacker(terminalData.id, hacker)
+							table.insert(safeHackers, hacker)
+							break
 						end
 					end
+				end
+			end
+
+			for _, hacker in pairs(terminalData.hackers) do
+				if not table.find(safeHackers, hacker) then
+					RoundDataManager.removeHackers(terminalData.id, hacker)
 				end
 			end
 		end
@@ -165,8 +209,23 @@ RunService.Heartbeat:Connect(function(dt)
 			terminalData.id,
 			dt * RoundConfiguration.terminalProgressPerSecondPerPlayer * numHackers
 		)
+		-- TODO: Score
 
-		if terminalData.progress >= 100 then continue end
+		if terminalData.progress >= 100 then
+			-- TODO: Score
+
+			if #terminalData.hackers > 0 then
+				RoundDataManager.removeHackers(terminalData.id, terminalData.hackers)
+			end
+
+			RoundDataManager.setTerminalStates(terminalData.id, {
+				isPuzzleMode = false,
+			})
+
+			table.clear(terminalData.puzzleQueue)
+
+			continue
+		end
 
 		terminalData.cooldown -= dt
 		if terminalData.cooldown <= 0 then
@@ -178,3 +237,39 @@ RunService.Heartbeat:Connect(function(dt)
 		end
 	end
 end)
+
+RoundDataManager.onPlayerStatusUpdated:Connect(function(playerData: RoundPlayerData)
+	if playerData.status == Enums.PlayerStatus.alive then return end
+
+	local player = Players:GetPlayerByUserId(playerData.playerId)
+
+	for _, terminalData in pairs(RoundDataManager.data.terminalData) do
+		if table.find(terminalData.hackers, player) then
+			RoundDataManager.removeHackers(terminalData.id, player)
+		end
+	end
+end)
+
+RoundDataManager.onPhaseChanged:Connect(function(phaseType)
+	if RoundConfiguration.roundPhases[phaseType] and not RoundConfiguration.terminalPhases[phaseType] then
+		for _, terminalData in pairs(RoundDataManager.data.terminalData) do
+			if #terminalData.hackers > 0 then
+				RoundDataManager.removeHackers(terminalData.id, terminalData.hackers)
+			end
+
+			if terminalData.progress ~= 100 then
+				RoundDataManager.setTerminalProgress(terminalData.id, 100)
+			end
+
+			if terminalData.isPuzzleMode then
+				RoundDataManager.setTerminalStates(terminalData.id, {
+					isPuzzleMode = false,
+				})
+			end
+
+			table.clear(terminalData.puzzleQueue)
+		end
+	end
+end)
+
+ClientServerCommunication.registerActionAsync("PromptTerminalPuzzle", onClientPuzzleResult)
