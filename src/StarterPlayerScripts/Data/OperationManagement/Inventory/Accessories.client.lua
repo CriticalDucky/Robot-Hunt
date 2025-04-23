94bca03a834135d5477d89bce5ecd37c893e1c95
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-04-23 02:23:46 -0400

    Migrate file

--- src/StarterPlayerScripts/Data/OperationManagement/Inventory/Accessories.client.lua
+++ src/StarterPlayerScripts/Data/OperationManagement/Inventory/Accessories.client.lua
@@ -4,10 +4,10 @@
 
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
 
-local replicatedStorageSharedData = ReplicatedStorage:WaitForChild("Shared"):WaitForChild "Data"
+local dataFolder = ReplicatedStorage:WaitForChild "Data"
 
-local ClientState = require(replicatedStorageSharedData:WaitForChild "ClientState")
-local ClientServerCommunication = require(replicatedStorageSharedData:WaitForChild "ClientServerCommunication")
+local ClientState = require(dataFolder:WaitForChild "ClientState")
+local ClientServerCommunication = require(dataFolder:WaitForChild "ClientServerCommunication")
 
 --#endregion
 
