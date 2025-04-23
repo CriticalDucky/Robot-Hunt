cc8d543ee819596b717d78609a7696daee8e945e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-04-23 02:19:30 -0400

    Add configuration files

--- src/ReplicatedFirst/Configuration/init.lua
+++ src/ReplicatedFirst/Configuration/init.lua
@@ -1,5 +1,7 @@
 local Configuration = {}
 
 Configuration.InterfaceConstants = require(script:WaitForChild("InterfaceConstants"))
+Configuration.PlayerDataConfig = require(script:WaitForChild("PlayerDataConfig"))
+Configuration.ClientInitiation = require(script:WaitForChild("ClientInitiation"))
 
 return Configuration
