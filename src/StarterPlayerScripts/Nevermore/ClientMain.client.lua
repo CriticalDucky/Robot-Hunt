643bc57b7a7ca142d67aa85aef8a1984ada4452a
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-05-04 23:09:56 -0400

    Add unused client main script for nervermore

---
+++ src/StarterPlayerScripts/Nevermore/ClientMain.client.lua
@@ -0,0 +1,19 @@
+-- local ReplicatedStorage = game:GetService("ReplicatedStorage")
+-- local RunService = game:GetService("RunService")
+
+-- local loader = ReplicatedStorage:WaitForChild("Nevermore"):WaitForChild("loader") :: ModuleScript
+-- local require = require(loader).bootstrapGame(loader.Parent)
+
+-- local serviceBag = require("ServiceBag").new()
+-- local ikServiceClient = serviceBag:GetService(require("IKServiceClient"))
+-- serviceBag:Init()
+-- serviceBag:Start()
+
+-- -- Configure
+-- ikServiceClient:SetLookAround(true)
+
+-- local IKAimPositionPriorites = require("IKAimPositionPriorites")
+
+-- -- RunService.Stepped:Connect(function()
+-- -- 	ikServiceClient:SetAimPosition(Vector3.zero, IKAimPositionPriorites.HIGH)
+-- -- end)
