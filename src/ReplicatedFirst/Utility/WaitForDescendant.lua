3cef0ea196b50ffb46701dace993af7afc9797e9
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-06-06 17:40:42 -0400

    Fix bug related to function handlers

--- src/ReplicatedFirst/Utility/WaitForDescendant.lua
+++ src/ReplicatedFirst/Utility/WaitForDescendant.lua
@@ -1,4 +1,4 @@
-local function waitForDescendant (descendantOf, identifier)
+local function waitForDescendant(descendantOf, identifier)
 	local idType = typeof(identifier)
 
 	assert(typeof(descendantOf) == "Instance", "Invalid type for argument 1 (descendatOf)")
@@ -6,39 +6,42 @@ local function waitForDescendant (descendantOf, identifier)
 
 	local TIMEOUT = 10
 
-	if idType == "string" and descendantOf:FindFirstChild(identifier, true) then
-		return descendantOf:FindFirstChild(identifier, true)
-	elseif idType == "function" then
+	if idType == "string" then
+		local found = descendantOf:FindFirstChild(identifier, true)
+		if found then return found end
+	else -- idType == "function"
 		for _, descendant in ipairs(descendantOf:GetDescendants()) do
-			if identifier(descendant) then -- that was the bug lol
-				return descendant
-			end
+			if identifier(descendant) then return descendant end
 		end
-
-		return
-	else
-		local object
-
-		task.spawn(function()
-			task.wait(TIMEOUT)
-
-			if not object then
-				warn("Infinite yield possible on "..tostring(descendantOf)..":WaitForDescendant("..tostring(identifier).."). Traceback:", debug.traceback())
-			end
-		end)
-
-		repeat
-			local descendant = descendantOf.DescendantAdded:Wait()
-
-			if idType == "string" then
-				object = if descendant.Name == identifier then descendant else nil
-			else -- strType == "function"
-				object = if identifier(descendant) then descendant else nil
-			end
-		until object
-
-		return object
 	end
+
+	local object
+
+	task.spawn(function()
+		task.wait(TIMEOUT)
+		if not object then
+			warn(
+				"Infinite yield possible on "
+					.. tostring(descendantOf)
+					.. ":WaitForDescendant("
+					.. tostring(identifier)
+					.. "). Traceback:",
+				debug.traceback()
+			)
+		end
+	end)
+
+	repeat
+		local descendant = descendantOf.DescendantAdded:Wait()
+
+		if idType == "string" then
+			object = if descendant.Name == identifier then descendant else nil
+		else -- idType == "function"
+			object = if identifier(descendant) then descendant else nil
+		end
+	until object
+
+	return object
 end
 
 _G.waitForDescendant = waitForDescendant
