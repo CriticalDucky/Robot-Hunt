42eb31d95a1e8dd34ffee7aa08890ee439a34aa6
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-06-08 15:59:02 -0400

    Add battery collision group

--- src/ServerScriptService/Management/Collisions.server.lua
+++ src/ServerScriptService/Management/Collisions.server.lua
@@ -2,6 +2,8 @@ local PhysicsService = game:GetService("PhysicsService")
 
 PhysicsService:RegisterCollisionGroup("Character")
 PhysicsService:RegisterCollisionGroup("CharacterDetection")
+PhysicsService:RegisterCollisionGroup("Battery")
 
 PhysicsService:CollisionGroupSetCollidable("Character", "Character", false)
 PhysicsService:CollisionGroupSetCollidable("Character", "CharacterDetection", false)
+PhysicsService:CollisionGroupSetCollidable("Character", "Battery", false)
